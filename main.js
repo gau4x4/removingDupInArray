02044abaf23680ae05b8941aee7ff051d718a4f4
gau4x4 <50134400+gau4x4@users.noreply.github.com>
2020-06-14 17:43:08 +0530

    Update main.js
    Adding Factorial program in it.

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 let dupRemoval=(testArr,flag)=>{
-  if(flag){
-    testArr.sort((a,b)=>a-b);
-    return Array.from(new Set(testArr));
-  }
-  else{
-     testArr.sort((a,b)=>b-a);
-     return Array.from(new Set(testArr));
-  }
+//   if(flag){
+//     testArr.sort((a,b)=>a-b);
+//     return Array.from(new Set(testArr));
+//   }
+//   else if(!flag){
+//      testArr.sort((a,b)=>b-a);
+//      return Array.from(new Set(testArr));
+//   }
+//   else{
+    if(flag===0){
+      return 1;
+    }
+     return flag*dupRemoval(testArr,flag-1);
+//   }
   
 }
 console.log(dupRemoval(new Array(1,1,2,3,5,6,6,7,9),9))
